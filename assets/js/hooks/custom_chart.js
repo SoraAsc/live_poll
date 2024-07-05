8040d334637e622e5f0510de531b99143c22237c
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2024-07-05 11:55:52 -0300

    feat: gráfico atualizado ao realizar votos

--- assets/js/hooks/custom_chart.js
+++ assets/js/hooks/custom_chart.js
@@ -7,24 +7,23 @@ export const CustomChart = {
     },
 
     updated() {
-        this.createChart()
+        this.updateChart()
     },
 
     createChart() {
         let ctx = this.el.getContext('2d')
         const type = this.el.getAttribute('data-type') || 'bar'
         const label = this.el.getAttribute('data-label') || 'Poll'
-        const labels = this.el.getAttribute('data-labels') || ["Poll"]
-        const data = this.el.getAttribute('data-dataset') || [0]
-        alert(data)
-        new Chart(ctx, {
+        const labels = JSON.parse(this.el.getAttribute('data-labels')) || ["Poll"]
+        const data = JSON.parse(this.el.getAttribute('data-dataset')) || [0]
+        this.chart = new Chart(ctx, {
             type,
             data: 
             {
                 labels: labels,//['January', 'February', 'March', 'April', 'May', 'June', 'July'],
                 datasets: [{
                   label: label,
-                  data: data,//[65, 59, 80, 81, 56, 55, 40],
+                  data: data,
                   backgroundColor: 'rgba(75, 192, 192, 0.2)',
                   borderColor: 'rgba(75, 192, 192, 1)',
                   borderWidth: 1
@@ -34,25 +33,19 @@ export const CustomChart = {
             {
                 scales: {
                   y: {
-                    beginAtZero: true
-                  }
+                    beginAtZero: true,
+                    ticks: {precision: 0}
+                  },
                 },
                 maintainAspectRatio: false,
                 responsive: false,
             }
         })
-        // let table = document.createElement("table")
-        // table.innerHTML = `
-        //     <tr class="bg-red-900">
-        //         <th>Header 1</th>
-        //         <th>Header 2</th>
-        //     </tr>
-        //     <tr>
-        //         <td>Data 1</td>
-        //         <td>Data 2</td>
-        //     </tr>
-        // `
-        // this.el.innerHTML = ""
-        // this.el.appendChild(table)
+    },
+
+    updateChart() {
+        const newDataset = JSON.parse(this.el.getAttribute('data-dataset')) || [0]
+        this.chart.data.datasets[0].data = newDataset
+        this.chart.update()
     }
 }
--- lib/live_poll_web/live/poll_live/result/poll_result_live.ex
+++ lib/live_poll_web/live/poll_live/result/poll_result_live.ex
@@ -1,18 +1,27 @@
 defmodule LivePollWeb.PollLive.Result.PollResultLive do
+  alias Phoenix.PubSub
   use LivePollWeb, :live_view
   import LivePoll.LivePolls
 
   def mount(%{"id" => id}, session, socket) do
+    if(connected?(socket)) do
+      PubSub.subscribe(LivePoll.PubSub, "vote_counter#{id}")
+    end
     user_ip = Map.get(session, "user_ip")
     results = get_poll_results(id)
     options_list = Enum.map(results, fn %{option_name: p1} -> p1 end)
     votes_list = Enum.map(results, fn %{vote_count: p2} -> p2 end)
-    # IO.inspect(options_list)
     {:ok, socket
       |> assign(:client_ip, user_ip)
       |> assign(:poll, get_poll!(id))
       |> assign(:options, options_list)
       |> assign(:votes, votes_list)
     }
+  end
+
+  def handle_info(:refresh_vote, socket) do
+    votes_list = Enum.map(get_poll_results(socket.assigns.poll.id), fn %{vote_count: p2} -> p2 end)
+    {:noreply, socket
+      |> assign(:votes, votes_list)}
   end
 end
